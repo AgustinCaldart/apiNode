169e2e52349cc65a21e5a2d2c0d1e24258dcf39d
acaldart <caldart123@gmail.com>
2021-10-29 04:54:35 -0300

    nuevas rutas

--- index.js
+++ index.js
@@ -6,6 +6,17 @@ app.get('/', (req, res) => {
   res.send('Hola mi server en express');
 }); // rutamos
 
+app.get('/nueva-ruta', (req, res) => {
+  res.send('nuevo endpoint');
+}); // creamos nueva ruta
+
+app.get('/products', (req, res) => {
+  res.json({
+    name: 'Product 1',
+    price: 1000,
+  }); //trabajamos con json porque somos api
+}); // creamos nueva ruta
+
 app.listen(port, () => {
   console.log('esta corriendo' + port);
 }); //prendemos server
