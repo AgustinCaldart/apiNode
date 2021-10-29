3c971e520f2de72081d46ade4a274f4796f18c12
acaldart <caldart123@gmail.com>
2021-10-29 06:20:58 -0300

    Ejemplos con faker

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express'); //usamos express
+const faker = require('faker'); //usamos faker
 const app = express();
 const port = 3000;
 
@@ -9,18 +10,19 @@ app.get('/', (req, res) => {
 app.get('/nueva-ruta', (req, res) => {
   res.send('nuevo endpoint');
 }); // creamos nueva ruta
-
+//usando faker
 app.get('/products', (req, res) => {
-  res.json([
-    {
-      name: 'Product 1',
-      price: 1000,
-    },
-    {
-      name: 'Product 2',
-      price: 2000,
-    },
-  ]); //trabajamos con json porque somos api
+  const products = [];
+  const { size } = req.query;
+  const limit = size || 10; //usamos los query para definir cantidad de productos
+  for (let index = 0; index < limit; index++) {
+    products.push({
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price(), 10),
+      image: faker.image.imageUrl(),
+    });
+  }
+  res.json(products); //trabajamos con json porque somos api
 }); // los get genericos son array
 
 //get de parametro con :
