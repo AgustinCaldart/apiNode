5ebf8c1f4653dc05e611ec332abbb3f47cf74f30
acaldart <caldart123@gmail.com>
2021-11-13 16:44:21 -0300

    pool categorias

--- services/categoriesService.js
+++ services/categoriesService.js
@@ -1,15 +1,18 @@
-const getConnection = require('../libs/postgres');
+const pool = require('../libs/postgresPool');
 
 class CategorieSerivce {
-  constructor() {}
+  constructor() {
+    this.pool = pool;
+    this.pool.on('error', (err) => console.log(err));
+  }
 
   async create(data) {
     return data;
   }
 
   async find() {
-    const client = await getConnection();
-    const rta = await client.query('SELECT * FROM categories');
+    const query = 'SELECT * FROM tasks';
+    const rta = await this.pool.query(query);
     return rta.rows;
   }
 
