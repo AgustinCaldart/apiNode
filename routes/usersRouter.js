6b920e11b5846b6940616c805ac7ba594fbafda5
acaldart <caldart123@gmail.com>
2021-11-13 13:05:43 -0300

    docker + postgres + user

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,18 +1,81 @@
-const express = require('express'); //usamos express
+const express = require('express');
+
+const UserService = require('./../services/userService');
+const validatorHandler = require('./../middleware/validatorHandler');
+const {
+  updateUserSchema,
+  createUserSchema,
+  getUserSchema,
+} = require('./../schemas/userSchemas');
 
 const router = express.Router();
-//parametros de tipo query
-router.get('/', (req, res) => {
-  //al ser opcionales no se ponen en la ruta
-  const { limit, offset } = req.query;
-  if (limit && offset) {
-    //clausula if ya que son opcionales
-    res.json({
-      limit,
-      offset,
-    });
-  } else {
-    res.send('No hay parametros');
+const service = new UserService();
+
+router.get('/', async (req, res, next) => {
+  try {
+    const users = await service.find();
+    res.json(users);
+  } catch (error) {
+    next(error);
   }
 });
+
+router.get(
+  '/:id',
+  validatorHandler(getUserSchema, 'params'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const category = await service.findOne(id);
+      res.json(category);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.post(
+  '/',
+  validatorHandler(createUserSchema, 'body'),
+  async (req, res, next) => {
+    try {
+      const body = req.body;
+      const newCategory = await service.create(body);
+      res.status(201).json(newCategory);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.patch(
+  '/:id',
+  validatorHandler(getUserSchema, 'params'),
+  validatorHandler(updateUserSchema, 'body'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const body = req.body;
+      const category = await service.update(id, body);
+      res.json(category);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.delete(
+  '/:id',
+  validatorHandler(getUserSchema, 'params'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      await service.delete(id);
+      res.status(201).json({ id });
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
 module.exports = router;
