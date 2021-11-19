c14f2f371905f96b30153e3f8a27193715dbed31
acaldart <caldart123@gmail.com>
2021-11-19 01:34:36 -0300

    arreglo en migracion

--- db/migrations/20211117032925-delete-categoryandproduct.js
+++ db/migrations/20211117032925-delete-categoryandproduct.js
@@ -2,11 +2,12 @@
 
 const { CATEGORY_TABLE } = require('../models/categoriesModel');
 const { PRODUCT_TABLE } = require('../models/productsModel');
-
+const { COSTUMER_TABLE } = require('../models/costumersModel');
 module.exports = {
   up: async (queryInterface) => {
     await queryInterface.dropTable(CATEGORY_TABLE);
     await queryInterface.dropTable(PRODUCT_TABLE);
+    await queryInterface.dropTable(COSTUMER_TABLE);
   },
 
   down: async (queryInterface, Sequelize) => {
