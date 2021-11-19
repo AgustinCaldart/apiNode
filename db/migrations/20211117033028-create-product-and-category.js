ea3fb3082aa03f96ed0fedfca2c0b66ac97287a9
acaldart <caldart123@gmail.com>
2021-11-19 01:36:06 -0300

    arreglo en migracion

--- db/migrations/20211117033028-create-product-and-category.js
+++ db/migrations/20211117033028-create-product-and-category.js
@@ -2,11 +2,11 @@
 
 const { CATEGORY_TABLE, CategorySchema } = require('../models/categoriesModel');
 const { ProductSchema, PRODUCT_TABLE } = require('../models/productsModel');
-
+const { COSTUMER_TABLE, CostumerSchema } = require('../models/costumersModel');
 module.exports = {
   up: async (queryInterface) => {
-    await queryInterface.createTable(CATEGORY_TABLE, CategorySchema);
     await queryInterface.createTable(PRODUCT_TABLE, ProductSchema);
+    await queryInterface.createTable(CATEGORY_TABLE, CategorySchema);
   },
 
   down: async (queryInterface) => {
