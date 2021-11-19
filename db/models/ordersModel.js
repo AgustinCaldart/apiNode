a85e3083adb1fed11b61012cda5e211dad9ec811
acaldart <caldart123@gmail.com>
2021-11-19 01:55:02 -0300

    virtual

--- db/models/ordersModel.js
+++ db/models/ordersModel.js
@@ -27,7 +27,7 @@ const OrderSchema = {
     onUpdate: 'CASCADE',
     onDelete: 'SET NULL',
   },
-  total: {
+  /*   total: {
     type: DataTypes.VIRTUAL,
     get() {
       if (this.items.length > 0) {
@@ -37,7 +37,7 @@ const OrderSchema = {
       }
       return 0;
     },
-  },
+  }, */
 };
 
 class Order extends Model {
