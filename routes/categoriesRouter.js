13684059abebef477cd254a8ef5b18c87725a428
acaldart <caldart123@gmail.com>
2021-11-27 16:05:11 -0300

    Firmando categorias

--- routes/categoriesRouter.js
+++ routes/categoriesRouter.js
@@ -7,6 +7,7 @@ const {
   createCategorySchema,
   getCategorySchema,
 } = require('./../schemas/categoriesSchemas');
+const passport = require('passport');
 
 const router = express.Router();
 const service = new CategoriesService();
@@ -36,6 +37,7 @@ router.get(
 
 router.post(
   '/',
+  passport.authenticate('jwt', { session: false }),
   validatorHandler(createCategorySchema, 'body'),
   async (req, res, next) => {
     try {
